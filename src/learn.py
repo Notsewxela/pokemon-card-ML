43f55b1892fa885d49e66209056316ab111c7410
Alex Weston <notsewxela@aol.com>
2022-11-20 16:39:18 +0000

    Initial model stuff

--- src/learn.py
+++ src/learn.py
@@ -1,7 +1,133 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 20 15:45:52 2022
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import tensorflow as tf
 
-@author: Alex
-"""
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
 
+import pathlib
+
+# TODO: Augment dataset
+
+
+# Size of batches of the data
+batch_size = 32
+# What our network should rescale our images to 
+img_height = 119 # default is 119 from our cropping code
+img_width = 198 # default is 198 from our cropping code
+
+data_dir = pathlib.Path("../data/")
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(class_names)
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+normalization_layer = layers.Rescaling(1./255)
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+# Notice the pixel values are now in `[0,1]`.
+print(np.min(first_image), np.max(first_image))
+
+num_classes = len(class_names)
+
+data_augmentation = keras.Sequential(
+  [
+    layers.RandomFlip("horizontal",
+                      input_shape=(img_height,
+                                  img_width,
+                                  3)),
+    layers.RandomFlip("vertical",
+                      input_shape=(img_height,
+                                  img_width,
+                                  3)),
+    layers.RandomRotation(0.1),
+  ]
+)
+
+model = Sequential([
+  data_augmentation,
+  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Dropout(0.2),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.summary()
+
+checkpoint_filepath = "../models/checkpoint"
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+  filepath=checkpoint_filepath,
+  monitor="val_accuracy",
+  mode="max",
+  save_best_only=True)
+
+
+epochs=10
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs,
+  callbacks=[model_checkpoint_callback]
+)
+
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(1, epochs + 1)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.xlabel("Epoch")
+plt.xlim(0.8, epochs+1.001)
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.xlabel("Epoch")
+plt.xlim(0.8, epochs+1.001)
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
